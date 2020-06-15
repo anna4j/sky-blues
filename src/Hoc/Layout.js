d7aed20ee82f1a4f6d5737dba09bbc52390172b9
anna4j <anushkajadhav004@gmail.com>
2020-06-15 21:33:02 +0530

    header

--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../Components/Header_footer/Header'
+
+
 
 const Layout = (props) => {
     return (
         <div>
-            hello
+            <Header />
             {props.children}
         </div>
     );
