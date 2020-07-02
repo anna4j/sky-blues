0e103762358d60289d4e0df40860ab15ae81992b
anna4j <anushkajadhav004@gmail.com>
2020-07-03 01:16:18 +0530

    work on home page

---
+++ src/Components/home/featured/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Featured = () => {
+    return (
+        <div className="featued_wrapper">
+            text
+        </div>
+    );
+};
+
+export default Featured;
